f930d7dab25f15771202542f556b67010ac84178
TSDT-2 <syua-02@mail.ru>
2024-09-24 01:32:56 +0400

    del
    вроде только удалены ненужные персонажи, но это не точно

--- Assets/Gameplay/GameCanvas/GameCanvas.cs
+++ Assets/Gameplay/GameCanvas/GameCanvas.cs
@@ -1,25 +1,34 @@
 using UnityEngine;
 using TMPro;
+using System;
 
 public class GameCanvas : MonoBehaviour
 {
     public TMP_Text levelTimeText;
+    public TMP_Text fireCrystalText;
+    public TMP_Text waterCrystalText;
+
 
     private float _levelTime;
+    private int _fireCrystalQuantity;
+    private int _waterCrystalQuantity;
+
 
     private void Start()
     {
         _levelTime = 0f;
-        UpdateLevelTimeText();
+        _fireCrystalQuantity = 0;
+        _waterCrystalQuantity = 0;
+        UpdateText();
     }
 
     private void Update()
     {
         _levelTime += Time.deltaTime;
-        UpdateLevelTimeText();
+        UpdateText();
     }
 
-    private void UpdateLevelTimeText()
+    private void UpdateText()
     {
         float minutes = Mathf.FloorToInt(_levelTime / 60);
         float seconds = Mathf.FloorToInt(_levelTime % 60);
@@ -29,6 +38,25 @@ public class GameCanvas : MonoBehaviour
     public void ResetTimer()
     {
         _levelTime = 0f;
-        UpdateLevelTimeText();
+        UpdateText();
+    }
+    public void AddFireCrystal()
+    {
+        _fireCrystalQuantity++;
+        UpdateFireCrystalCountText();
+    }
+    public void AddWaterCrystal()
+    {
+        _waterCrystalQuantity++;
+        UpdateWaterCrystalCountText();
+    }
+
+    private void UpdateFireCrystalCountText()
+    {
+        fireCrystalText.text = Convert.ToString(_fireCrystalQuantity);
+    }
+    private void UpdateWaterCrystalCountText()
+    {
+        waterCrystalText.text = Convert.ToString(_waterCrystalQuantity);
     }
 }
