99bf2a502cc002dc35a9274dab48ebfa310555cc
TSDT-2 <syua-02@mail.ru>
2024-09-27 02:02:17 +0400

    add restart canvas
    uh...

--- Assets/Gameplay/Fire/Scripts/Fire.cs
+++ Assets/Gameplay/Fire/Scripts/Fire.cs
@@ -1,12 +1,42 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Fire : MonoBehaviour
 {
+    [SerializeField] private GameObject _gameOverCanvas;
+    [SerializeField] private Button _restartLvl;
+    [SerializeField] private Button _selectGame;
+    private void Start()
+    {
+        _gameOverCanvas.SetActive(false);
+        _restartLvl.onClick.AddListener(RestartLevel);
+        _selectGame.onClick.AddListener(SelectLevel);
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("WaterCharacter"))
         {
             Destroy(other.gameObject);
+            ShowGameOver();
         }
     }
+
+    public void ShowGameOver()
+    {
+        Time.timeScale = 0;
+        _gameOverCanvas.SetActive(true);
+    }
+
+    public void RestartLevel()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void SelectLevel()
+    {
+        SceneManager.LoadScene("MainMenu");// должна быть другая сцена
+    }
+    
 }
