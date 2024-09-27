728090b1d4d4941b163e0b53f87f9c0c99792c5c
TSDT-2 <syua-02@mail.ru>
2024-09-27 16:51:57 +0400

    add panel

--- Assets/Gameplay/Other/Leverage/LeverController.cs
+++ Assets/Gameplay/Other/Leverage/LeverController.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class LeverController : MonoBehaviour
 {
-    private enum LeverState { Inactive, Active }
-    private LeverState currentState = LeverState.Inactive;
+    public enum LeverState { Inactive, Active }
+    public LeverState _currentState = LeverState.Inactive;
 
     public float activeRotationAngle = 45f;
     public float inactiveRotationAngle = -45f;
     public float rotationSpeed = 5f;
-    public float forceThreshold = 1f; // Порог силы для переключения состояния
+    public float forceThreshold = 1f;
 
     [SerializeField] private Transform pivot;
 
@@ -28,20 +28,20 @@ public class LeverController : MonoBehaviour
 
     private void ToggleLever()
     {
-        currentState = currentState == LeverState.Inactive ? LeverState.Active : LeverState.Inactive;
+        _currentState = _currentState == LeverState.Inactive ? LeverState.Active : LeverState.Inactive;
         PerformActionBasedOnState();
     }
 
     private void Update()
     {
-        float targetAngle = currentState == LeverState.Active ? activeRotationAngle : inactiveRotationAngle;
+        float targetAngle = _currentState == LeverState.Active ? activeRotationAngle : inactiveRotationAngle;
         float angle = Mathf.LerpAngle(pivot.eulerAngles.z, targetAngle, Time.deltaTime * rotationSpeed);
         pivot.eulerAngles = new Vector3(0, 0, angle);
     }
 
     private void PerformActionBasedOnState()
     {
-        Debug.Log("Lever is now " + (currentState == LeverState.Active ? "Active" : "Inactive"));
+        Debug.Log("Lever is now " + (_currentState == LeverState.Active ? "Active" : "Inactive"));
     }
 }
     
